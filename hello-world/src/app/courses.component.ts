2a124d46b73325b83dee685cab97ba79826b3971
Jumanazar Saidov <jumsai@attolabs.de>
2023-12-10 16:06:47 +0500

    Added some use of directive such as *ngFor for looping a list.

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,8 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+      <h2>{{ getTitle() }}</h2>
+      <ul *ngFor="let item of courses">
+        <li>{{item}}</li>
+      </ul>
+    `
 })
 export class CoursesComponent{
+  title = "List of courses";
+  courses = ["course1","course2","course3"];
 
+  getTitle() {
+    return this.title;
+    }
 }
