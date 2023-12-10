0d3f06106fa06baa234fbb8e83495bbc9c71da12
Jumanazar Saidov <jumsai@attolabs.de>
2023-12-10 16:00:28 +0500

    Added a course component using cli command: 'ng g c course'.

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
