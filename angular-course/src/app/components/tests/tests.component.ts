cd4c83ee480086c313a1bdca67a3593cc3854e7f
jimhome_mac <jsr1611@outlook.com>
2023-12-22 01:07:27 +0500

    Adding test logic

--- angular-course/src/app/components/tests/tests.component.ts
+++ angular-course/src/app/components/tests/tests.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class TestsComponent {
 
+  testWord:any = "saljun";
+  answers:any = ['a', 'b', 'c', 'd'];
+  correctAnswer:number = 2;
+
+  submitAnswer(key: number){
+    let btn = document.getElementById('ans'+key);
+    btn?.classList.remove('btn-light');
+    if(key === this.correctAnswer){
+      btn?.classList.add('btn-success');
+    }else{
+      btn?.classList.add('btn-danger');
+    }
+  }
 }
