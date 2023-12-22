b450423f468ad62149a1bad961386e9e4f37111c
Jumanazar Saidov <jumsai@attolabs.de>
2023-12-22 10:39:36 +0500

    Set up netlify ci/cd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-course'`, () => {
+  it(`should have as title 'arabic_frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-course');
+    expect(app.title).toEqual('arabic_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-course app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('arabic_frontend app is running!');
   });
 });
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarService } from './navbar.service';
 
 @Component({
@@ -6,9 +6,8 @@ import { NavBarService } from './navbar.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent{
   constructor(private navbarService: NavBarService){ }
-
   useLocalDB(){
     let btn = document.getElementById("localdb");
     let iTag = btn?.querySelector('i');
